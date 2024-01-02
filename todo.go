982b9309e49df9b9d689861be6257170a2a0b804
Elviie Abdulmennanova <el123abd@gmail.com>
2024-01-02 16:17:02 -0500

    add methods Add and Complete to List type

---
+++ todo.go
@@ -0,0 +1,41 @@
+package todo
+
+import (
+	"fmt"
+	"time"
+)
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type List []item
+
+// Add creates a new todo item and appends it to the list
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*l = append(*l, t)
+}
+
+// Complete method marks a todo item as completed by
+// Done = true, CompletedAt = current time
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
