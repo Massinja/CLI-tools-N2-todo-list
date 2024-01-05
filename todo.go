2a6492eb56d7109237f808f5c3874b1976a8a5fa
Elviie Abdulmennanova <el123abd@gmail.com>
2024-01-05 18:03:11 -0500

    attach delete, save, get methods to the type List

--- todo.go
+++ todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"os"
 	"time"
 )
 
@@ -13,6 +16,13 @@ type item struct {
 }
 
 type List []item
+
+func checkErr(e error) {
+	if e != nil {
+		fmt.Errorf("%v", e)
+		return
+	}
+}
 
 // Add creates a new todo item and appends it to the list
 func (l *List) Add(task string) {
@@ -36,6 +46,46 @@ func (l *List) Complete(i int) error {
 
 	ls[i-1].Done = true
 	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+// Delete removes a todo item from the list
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	*l = append(ls[:i-1], ls[i:]...)
+	return nil
+
+}
+
+// Save encodes the List as JSON and saves it
+func (l *List) Save(filename string) error {
+
+	lj, err := json.Marshal(l)
+	checkErr(err)
+
+	er := os.WriteFile(filename, lj, 0644)
+	checkErr(er)
+
+	return nil
+}
+
+// Get opens the file, decodes JSON and parses it into a List
+func (l *List) Get(filename string) error {
+
+	f, err := os.Open(filename)
+	checkErr(err)
+
+	file, err := io.ReadAll(f)
+	checkErr(err)
+	defer f.Close()
+
+	er := json.Unmarshal(file, l)
+	checkErr(er)
 
 	return nil
 }
