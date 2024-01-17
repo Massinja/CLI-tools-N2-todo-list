bc6028d9b267334297422d0fa2a9fe2083d1b24e
Elviie Abdulmennanova <el123abd@gmail.com>
2024-01-16 21:39:55 -0500

    add getTask function

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 	"todo"
 )
 
 var todoFileName = "todo.json"
+
+// getTask decides where to get the description for a new task from: arguments o STDIN
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, ""), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+	return s.Text(), nil
+}
 
 func main() {
 	// Check if the user defined the ENV VAR for a custom file name
